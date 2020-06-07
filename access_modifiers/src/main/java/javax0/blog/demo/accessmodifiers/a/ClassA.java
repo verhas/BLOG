ed5318958f992c5b1f3cb309b9da3a35aea9d994
Peter Verhas <peter@verhas.com>
2020-06-07 10:42:56 +0200

    formatted

--- access_modifiers/src/main/java/javax0/blog/demo/accessmodifiers/a/ClassA.java
+++ access_modifiers/src/main/java/javax0/blog/demo/accessmodifiers/a/ClassA.java
@@ -2,15 +2,16 @@ package javax0.blog.demo.accessmodifiers.a;
 
 class ClassA {
 
-        public void publicMethod() {
-            System.out.println("publicMethod");
-        }
-    public static void main(String[] argv){
+    public void publicMethod() {
+        System.out.println("publicMethod");
+    }
+
+    public static void main(String[] argv) {
         var sut = new ClassA();
         sut.publicMethod();
     }
 
-    public static ClassA makeA(){
+    public static ClassA makeA() {
         return new ClassA();
     }
 }
