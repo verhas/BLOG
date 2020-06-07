8bb3589eff765d99336483fd00a8179a2077024e
Peter Verhas <peter@verhas.com>
2020-06-07 08:55:02 +0200

    3points demo with the errors

--- access_modifiers/src/main/java/points/Point.java
+++ access_modifiers/src/main/java/points/Point.java
@@ -3,7 +3,7 @@ package points;
 public class Point {
     protected int x, y;
     void warp(threePoint.Point3d a) {
-        //if (a.z > 0) // compile-time error: cannot access a.z
+        if (a.z > 0) // compile-time error: cannot access a.z
             a.delta(this);
     }
 }
--- access_modifiers/src/main/java/threePoint/Point3d.java
+++ access_modifiers/src/main/java/threePoint/Point3d.java
@@ -6,8 +6,8 @@ public class Point3d extends Point {
     protected int z;
 
     public void delta(Point p) {
-        //p.x += this.x; // compile-time error: cannot access p.x
-        //p.y += this.y; // compile-time error: cannot access p.y
+        p.x += this.x; // compile-time error: cannot access p.x
+        p.y += this.y; // compile-time error: cannot access p.y
     }
 
     public void delta3d(Point3d q) {
